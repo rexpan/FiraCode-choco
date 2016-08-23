153d48173cc605af07a5f3133fefad0e31a8cc1d
Rex Pan <napxer@gmail.com>
2016-08-23 11:06:17 +0700

    Update chocolateyInstall.ps1 to add $checksum

--- FiraCode/tools/chocolateyInstall.ps1
+++ FiraCode/tools/chocolateyInstall.ps1
@@ -1,16 +1,40 @@
-$package = 'FiraCode'
+function Get-CurrentDirectory
+{
+  $thisName = $MyInvocation.MyCommand.Name
+  [IO.Path]::GetDirectoryName((Get-Content function:$thisName).File)
+}
+
+$fontHelpersPath = (Join-Path (Get-CurrentDirectory) 'FontHelpers.ps1')
+. $fontHelpersPath
 
 $fontUrl = 'https://github.com/tonsky/FiraCode/releases/download/1.200/FiraCode_1.200.zip'
+$checksumType = 'sha256';
+$checksum = 'A72A8C2453D9F57A651ABAAC8148B5850AD04E4A866C9E3616BD0FE8B43B4AFE';
+
 $destination = Join-Path $Env:Temp 'FiraCode'
 
-Install-ChocolateyZipPackage -PackageNam $package -url $fontUrl -unzipLocation $destination
+Install-ChocolateyZipPackage -PackageName 'FiraCode' -url $fontUrl -unzipLocation $destination -ChecksumType "$checksumType" -Checksum "$checksum"
 
 $shell = New-Object -ComObject Shell.Application
 $fontsFolder = $shell.Namespace(0x14)
 
 $fontFiles = Get-ChildItem $destination -Recurse -Filter *.otf
 
+# unfortunately the font install process totally ignores shell flags :(
+# http://social.technet.microsoft.com/Forums/en-IE/winserverpowershell/thread/fcc98ba5-6ce4-466b-a927-bb2cc3851b59
+# so resort to a nasty hack of compiling some C#, and running as admin instead of just using CopyHere(file, options)
+$commands = $fontFiles |
+% { Join-Path $fontsFolder.Self.Path $_.Name } |
+? { Test-Path $_ } |
+% { "Remove-SingleFont '$_' -Force;" }
+
+# http://blogs.technet.com/b/deploymentguys/archive/2010/12/04/adding-and-removing-fonts-with-windows-powershell.aspx
 $fontFiles |
-    ForEach-Object { $fontsFolder.CopyHere($_.FullName) }
+% { $commands += "Add-SingleFont '$($_.FullName)';" }
+
+$toExecute = ". $fontHelpersPath;" + ($commands -join ';')
+Start-ChocolateyProcessAsAdmin $toExecute
 
 Remove-Item $destination -Recurse
+
+
